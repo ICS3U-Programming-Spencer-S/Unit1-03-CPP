06e2de692a481d59640b47e2790df1af18feae5e
Spencer Scarlett <Spencer.Scarlett@stu.ocsb.ca>
2022-09-14 23:07:33 +0000

    Added helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Ms Raffin All rights reserved.
+// .
+// Created by: Spencer Scarlett
+// Date: 9/14/2022
+// This program displays "Hello, World with style in C++".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style, but in C++!" << std::endl;
+    std::cout << "How cool is that!" << std::endl;
+}
